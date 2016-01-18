032f20fceae0e888630cb59e65ee1ee7d1fb4e98
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-01-18 21:50:11 +0800

    test beside.js

---
+++ __tests__/beside-test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+jest
+  .dontMock('../src/js/beside');
+
+describe('beside', function() {
+  var beside = require('../src/js/beside');
+  var instance = require('../src/js/instance');
+
+  it('init in instance should called', function() {
+    document.body.innerHTML = '<div id="me">ME</div><div id="you">YOU</div>';
+
+    beside.init({
+      me: document.getElementById('me'),
+      you: document.getElementById('you'),
+      where: 'top center'
+    });
+
+    expect(instance.init).toBeCalledWith({
+      me: document.getElementById('me'),
+      you: document.getElementById('you'),
+      where: 'top center'
+    });
+
+  });
+
+});
